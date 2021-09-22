5e083ad5351be4d334dd6d1f839ebec4a756e040
松本公彰 <gph20078@s.okadai.jp>
2021-09-22 23:01:03 +0900

    メイン部分のレスポンシブtab1、tab2を作成した

--- js/main.js
+++ js/main.js
@@ -1,16 +1,13 @@
 $(function () {
-  
   // ハンバーガーメニューの実装
-  $("#hamburger_button").click(function () {
-    $(this).toggleClass("active");
+  $(".js-hamburger").click(function () {
+    $(this).toggleClass("is-open");
     $(".p-Nav__body").slideToggle(200);
   });
 
   // アコーディオンメニューの実装
-  $(".has-sub").hover(function () {
-    $(this).children(".sub-menu").stop().slideToggle(300);
-    $(this).toggleClass("open");
-    $(".has-sub").not(this).removeClass("open");
-    $(".has-sub").not($(this)).next(".sub-menu").slideUp();
+  $(".menu-item").hover(function () {
+    $(this).children(".sub-menu").stop().slideToggle(200);
+    $(this).find(".has-sub").toggleClass("open");
   });
 });
